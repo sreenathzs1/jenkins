852f83a5749cd01efb11f532adb3736c9c95bece
sreenath <sreenathkk@gmail.com>
2021-05-12 11:24:06 +0530

    #

--- seed.groovy
+++ seed.groovy
@@ -1,4 +1,4 @@
-pipeline('frontend-ci') {
+pipelineJob('frontend-ci') {
  configure { flowdefinition ->
     flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git'){
