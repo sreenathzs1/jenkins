6933d9085f8dc843583333b78188be9c77c2389a
sreenath <sreenathkk@gmail.com>
2021-05-18 16:58:34 +0530

    1

--- seed.groovy
+++ seed.groovy
@@ -23,3 +23,24 @@ pipelineJob('CI-Pipelines/frontend-ci') {
     }
  }
 }
+
+pipelineJob('CI-Pipelines/users-ci') {
+ configure { flowdefinition ->
+    flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git'){
+            'userRemoteConfigs' {
+                'hudson.plugins.git.UserRemoteConfig' {
+                    'url'('https://github.com/sreenathzs1/users.git')
+                }
+            }
+            'branches' {
+                'hudson.plugins.git.BranchSpec' {
+                    'name'('*/main')
+                }
+            }
+        }
+        'scriptpath'('jenkinsfile')
+        'lightweight'(true)
+    }
+ }
+}
