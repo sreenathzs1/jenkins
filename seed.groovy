fc70ee003032e2fd905545294793c415445bd915
sreenath <sreenathkk@gmail.com>
2021-05-18 18:01:04 +0530

    1

--- seed.groovy
+++ seed.groovy
@@ -44,3 +44,66 @@ pipelineJob('CI-Pipelines/users-ci') {
     }
  }
 }
+
+pipelineJob('CI-Pipelines/login-ci') {
+ configure { flowdefinition ->
+    flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git'){
+            'userRemoteConfigs' {
+                'hudson.plugins.git.UserRemoteConfig' {
+                    'url'('https://github.com/sreenathzs1/login.git')
+                }
+            }
+            'branches' {
+                'hudson.plugins.git.BranchSpec' {
+                    'name'('*/main')
+                }
+            }
+        }
+        'scriptpath'('Jenkinsfile')
+        'lightweight'(true)
+    }
+ }
+}
+
+pipelineJob('CI-Pipelines/todo-ci') {
+ configure { flowdefinition ->
+    flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git'){
+            'userRemoteConfigs' {
+                'hudson.plugins.git.UserRemoteConfig' {
+                    'url'('https://github.com/sreenathzs1/todo.git')
+                }
+            }
+            'branches' {
+                'hudson.plugins.git.BranchSpec' {
+                    'name'('*/main')
+                }
+            }
+        }
+        'scriptpath'('Jenkinsfile')
+        'lightweight'(true)
+    }
+ }
+}
+
+pipelineJob('CI-Pipelines/redis-ci') {
+ configure { flowdefinition ->
+    flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git'){
+            'userRemoteConfigs' {
+                'hudson.plugins.git.UserRemoteConfig' {
+                    'url'('https://github.com/sreenathzs1/redis.git')
+                }
+            }
+            'branches' {
+                'hudson.plugins.git.BranchSpec' {
+                    'name'('*/main')
+                }
+            }
+        }
+        'scriptpath'('Jenkinsfile')
+        'lightweight'(true)
+    }
+ }
+}
