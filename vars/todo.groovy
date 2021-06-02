166332e29e82597e277716488b9d2f97a7531ae9
sreenath <sreenathkk@gmail.com>
2021-06-02 13:23:18 +0530

    1

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -13,9 +13,9 @@ pipeline {
     triggers {
         pollSCM('*/2 * * * 1-5')
     }
-    tools {
-        maven 'mvn3.6.3'
-    }
+    //tools {
+      //  maven 'mvn3.6.3'
+    //}
 
     environment {
         COMPONENT       = "${args.COMPONENT}"
