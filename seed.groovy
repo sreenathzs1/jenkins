b5e248fd36278f21904d13f913f9cc54aaa6ba5e
sreenathzs <sreenathzs@zelarsoft.com>
2021-07-30 16:40:45 +0530

    s

--- seed.groovy
+++ seed.groovy
@@ -46,7 +46,7 @@ pipelineJob("Deployment Pipeline") {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git'){
             'userRemoteConfigs' {
                 'hudson.plugins.git.UserRemoteConfig' {
-                    'url'('https://github.com/sreenathzs1/jenkins.git')
+                    'url'(['https://github.com/sreenathzs1/jenkins.git', 'credentialsId': 'GitHub_Credentials'])
                 }
             }
             'branches' {
